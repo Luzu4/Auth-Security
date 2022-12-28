0220fd9cc11286725e57ce4a2b16bc0e3fd3c4ac
Luzu4 <maciekluz@gmail.com>
2022-12-28 23:27:50 +0100

    nothing new

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname + '/public')));
 app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({
     extended: true
 }));
